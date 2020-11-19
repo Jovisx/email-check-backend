86a3b21d0420494470d9970f6f307ab1d6f8a583
Jovisx <wakeappltd@gmail.com>
2020-11-19 21:51:21 +1000

    fixed cors problem

--- src/utils/cors.js
+++ src/utils/cors.js
@@ -1,8 +1,7 @@
 module.exports = {
   ALLOW_CORS: {
-    'Access-Control-Allow-Credentials': true,
-    'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, PATCH, OPTIONS',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization, ApiKey, Signature',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin,X-Requested-With,Content-Type,Accept,Authorization,ApiKey,Signature,X-Amz-Date,X-Api-Key,X-Amz-Security-Token',
     'Access-Control-Allow-Origin': '*'
   }
 };
